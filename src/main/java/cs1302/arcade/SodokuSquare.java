bd410834a832ad8e323f8b220cb271a87e892202
Will McCarthy <36211066+Will-McCarthy@users.noreply.github.com>
2018-12-10 15:37:38 -0500

    Javadoc added to SodokuSquare

--- src/main/java/cs1302/arcade/SodokuSquare.java
+++ src/main/java/cs1302/arcade/SodokuSquare.java
@@ -6,12 +6,21 @@ import javafx.scene.text.Font;
 import javafx.scene.paint.Color;
 import javafx.geometry.Insets;
 import javafx.scene.layout.Pane;
-
+/**
+* sodokuSquare is kind of all of the backend and board for our sodoku game.
+* It makes the board squares and sets them to change when clicked. 
+* */
 public class SodokuSquare extends GridPane{
     private SodokuSquare[][] board = new SodokuSquare[3][];
 
     private Text[][] arr = new Text[3][];
-
+/**
+* This constructor makes a 3x3 box of text objects. We make 9 of these to make the
+* whole board.
+* @param mini Mini is the array which this sodokuSquare will represent
+* @param sodoku Sodoku is the game that made this object-lets it get numSetting
+* @param dontUse param is just here to differentiate this contstructor from the other
+* */
     private SodokuSquare(int[][] mini, Sodoku sodoku, int dontUse){
 	setStyle("-fx-background-color: black;");
 	for(int i = 0; i<3; i++){
@@ -38,11 +47,15 @@ public class SodokuSquare extends GridPane{
 			add(temp2, i, j);
 		}//for j
 	}//for i
-	//setHgap(15);
-	//setVgap(5);
     }//constructor
 
-
+/** 
+* This is the constructor called by the Sodoku class. It makes each of the large blocks
+* that each hold 9 numbers. 
+*
+* @param init init the initial board in an array
+* @param sodoku This is the game calling the method
+* */
     public SodokuSquare(int[][] init, Sodoku sodoku){
 	setStyle("-fx-background-color: white;");
 	for(int i = 0; i<3; i++){
@@ -53,10 +66,16 @@ public class SodokuSquare extends GridPane{
 			add(board[i][j], i, j);
 		}//for j
 	}//for i
-	//setHgap(20);
-	//setVgap(10);
-    }
-    
+    }//sodokuSquare
+	
+/** 
+* When sodokuSquare gets the inital board, this method breaks it up into each of
+* of the 3x3 blocks.
+*
+* @param init the whole starting board
+* @param y the first coordinate of the set of 9
+* @param x the second coordinate of the set of 9
+* */
     private int[][] miniArr(int[][] init, int y, int x){
 
 	int[][] mini = new int[3][];
