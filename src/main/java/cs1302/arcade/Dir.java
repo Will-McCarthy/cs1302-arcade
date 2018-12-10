5dd951e628c5984c126ab66be341b14e7c06fe30
Will McCarthy <36211066+Will-McCarthy@users.noreply.github.com>
2018-12-10 15:56:23 -0500

    Javadoc comments added

--- src/main/java/cs1302/arcade/Dir.java
+++ src/main/java/cs1302/arcade/Dir.java
@@ -1,21 +1,35 @@
 package cs1302.arcade;
 
+/**
+* stores the direction the snake is traveling
+* */
 public class Dir{
 
     int x = 0;
     int y = 0;
-    
+/**
+* changes direction twards i, j
+* @param i x direction component
+* @param j y direction component
+* */
     public void changeDir(int i, int j){
 	x = i;
 	y = j;
-
     }//changeDir
+/** 
+* gets x direction
+* @return x component direction in x
+* */
     public int getX(){
 	return x;
-    }
+    }//getX
+	
+/** 
+* gets y direction
+* @return y component direction in y
+* */
     public int getY(){
 	return y;
-    }
-
-    
+    }//getY
+	
 }//Dir
