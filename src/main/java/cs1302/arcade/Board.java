1befed05d247863f8c5888f63e081f70d954a07e
Will McCarthy <36211066+Will-McCarthy@users.noreply.github.com>
2018-12-10 15:42:33 -0500

    javadoc comments

--- src/main/java/cs1302/arcade/Board.java
+++ src/main/java/cs1302/arcade/Board.java
@@ -5,12 +5,18 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 
-
+/**
+* board is the board for snake. Its just a bunch of rectangles
+* that hold white, green or black.
+* */
 public class Board extends GridPane {
     
     private Box[][] squares;
     private double squareSize = 14;
-
+/**
+* Makes a new board to specified length.
+* @param rowLength Number of rows and columns on board
+* */
     public Board(int rowLength){	
 	squares = new Box[rowLength][];	
 	for(int i=0; i<rowLength; i++){
@@ -21,7 +27,10 @@ public class Board extends GridPane {
 		}//for j
 	}//for i
     }//constructor
-	       
+/** 
+* just returns the box specified
+* @return Box box specified
+* */
     public Box getBox(int i, int j){
 	return squares[i][j];
     }//getBox
